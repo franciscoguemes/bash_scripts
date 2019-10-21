d9fa799af3d86db4dd3f22de2e6c85fc14856093
Francisco Güemes <francisco.gueemes@uniscon.net>
2019-10-21 21:59:23 +0200

    Scan documents into *.tiff images

---
+++ bash/scan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Command: ./scan.sh fichero_salida.tif
+# Example: ./scan.sh myPassport.tif
+
+# If you do not have the file /usr/share/color/icc/RGB/AdobeRGB1998.icc your can download it from:
+#   https://supportdownloads.adobe.com/product.jsp?product=62&platform=Windows
+
+scanimage --format=tiff    \
+            --mode Color     \
+            --depth 8        \
+            --resolution 300 \
+            --icc-profile /usr/share/color/icc/RGB/AdobeRGB1998.icc \
+             -l 1mm          \
+             -t 1mm          \
+             -x 208mm        \
+             -y 295mm        \
+             -v              \
+             > $1.tif
