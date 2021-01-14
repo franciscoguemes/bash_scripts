ec4855a14260468f51fac861e13cc24772657012
Francisco Güemes <francisco.gueemes@uniscon.com>
2021-01-14 01:55:47 +0100

    Modified LAMP scripts to make them generic to any project

--- bash/start_docker_LAMP.sh
+++ bash/start_docker_LAMP.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 #############################################################################################
+# Usage:
+# 	start_docker_LAMP.sh [directory_www]
 #
 # Description:
+#	The optional argument "directory_www" is the directory that the Apache server will serve as content directory.
+#
 #	The script starts the docker-compose-lamp. The server will serve by default the directory "www"
 #	inside its directory structure. So this script first mounts a development directory as the "www" directory,
 #	by doing this it is ensured that the server serves the development content and a lot of copy/paste is avoided.
@@ -15,7 +19,7 @@
 #	by the filesystem. 
 #	
 # Referenced by:
-#	
+#	/home/francisco/git/Others/docker-compose-lamp/docker-compose-lamp/README.md
 #
 # Depends on:
 #	stop_docker_LAMP.sh
@@ -26,8 +30,24 @@
 #
 #############################################################################################
 
-sudo mount -o bind /home/francisco/git/Francisco/gitlab/mywebsite /home/francisco/git/Others/docker-compose-lamp/docker-compose-lamp/www
+LAMP_HOME=/home/francisco/git/Others/docker-compose-lamp/docker-compose-lamp
+LAMP_WWW=$LAMP_HOME/www
+
+# If an argument is supplied then try to mount the given directory as the www directory of the Apache server.
+if [ "$#" -eq  "1" ]
+then
+    # Test that the given directory exists...	
+    if [[ ! -d $1 ]]
+    then
+        echo "The given directory ${1} does not exist!"
+    exit 1
+    fi
+ 
+    # Mount the given directory in the place of the LAMP_WWW directory.
+    sudo mount -o bind $1 $LAMP_WWW
+fi
+
 sleep 0.5
-cd /home/francisco/git/Others/docker-compose-lamp/docker-compose-lamp
+cd $LAMP_HOME
 
 sudo docker-compose start
--- bash/stop_docker_LAMP.sh
+++ bash/stop_docker_LAMP.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 #############################################################################################
+# Usage:
+# 	stop_docker_LAMP.sh [directory_www]
 #
 # Description:
+#	The optional argument "directory_www" is the directory that the Apache is serving as content directory and it was supplied
+#	in the equivalent script start_docker_LAMP.sh
+#
 #	The script stops the docker-compose-lamp. First it stops the server and then it unmounts the development directory
-#	that was mounted in place of the "www" directory.
+#	that was mounted in place of the "www" directory (if any).
 #	
 #	Note that the original contents of the directory "www" are not accessible until the development directory
 #	is unmounted. But once the development directory is umnomted, the original contents of "www" will be accesible again
@@ -14,7 +19,7 @@
 #	The script stops the docker-compose-lamp
 #	
 # Referenced by:
-#
+#	/home/francisco/git/Others/docker-compose-lamp/docker-compose-lamp/README.md
 #
 # Depends on:
 #	start_docker_LAMP.sh
@@ -25,8 +30,26 @@
 #
 #############################################################################################
 
-cd /home/francisco/git/Others/docker-compose-lamp/docker-compose-lamp
+LAMP_HOME=/home/francisco/git/Others/docker-compose-lamp/docker-compose-lamp
+LAMP_WWW=$LAMP_HOME/www
+
+cd $LAMP_HOME
 
 sudo docker-compose stop
 sleep 0.5
-sudo umount /home/francisco/git/Francisco/gitlab/mywebsite /home/francisco/git/Others/docker-compose-lamp/docker-compose-lamp/www
+
+# If an argument is supplied then try to umount the given directory as the www directory of the Apache server.
+if [ "$#" -eq  "1" ]
+then
+    # Test that the given directory exists...	
+    if [[ ! -d $1 ]]
+    then
+        echo "The given directory ${1} does not exist!"
+    exit 1
+    fi
+ 
+    # Umount the given directory in the place of the LAMP_WWW directory.
+    sudo umount -$1 $LAMP_WWW
+fi
+
+
