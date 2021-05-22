637dea47b2516730006e2cc2f53f07f02a7405e6
Francisco Güemes <francisco.gueemes@uniscon.com>
2021-05-23 00:41:33 +0200

    Modify the template bash_script.sh

--- new_structure/customize/templates/bash_script.sh
+++ new_structure/customize/templates/bash_script.sh
@@ -6,12 +6,73 @@
 #Args           :                                                                                           
 #Author       	: Francisco Güemes                                                
 #Email         	: francisco@franciscoguemes.com                                           
-#See also	    : 
-#
+#See also	    : https://devhints.io/bash
+#                 https://linuxhint.com/30_bash_script_examples/
+#                 https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash  
 ####################################################################################################
 
 set -ex
 
+
+OPTIONS=dfo:v
+LONGOPTS=debug,force,output:,verbose
+
+function parse_arguments {
+    ! getopt --test > /dev/null 
+    if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+        echo 'I’m sorry, `getopt --test` failed in this environment.'
+        exit 1
+    fi
+
+    ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        # e.g. return value is 1
+        #  then getopt has complained about wrong arguments to stdout
+        exit 2
+    fi
+
+    eval set -- "$PARSED"
+
+    d=n f=n v=n outFile=-
+    # now enjoy the options in order and nicely split until we see --
+    while true; do
+        case "$1" in
+            -d|--debug)
+                d=y
+                shift
+                ;;
+            -f|--force)
+                f=y
+                shift
+                ;;
+            -v|--verbose)
+                v=y
+                shift
+                ;;
+            -o|--output)
+                outFile="$2"
+                shift 2
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                echo "Programming error"
+                exit 3
+                ;;
+        esac
+    done
+
+    # handle non-option arguments
+    if [[ $# -ne 1 ]]; then
+        echo "$0: A single input file is required."
+        exit 4
+    fi
+
+
+
+}
 
 
 #–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
