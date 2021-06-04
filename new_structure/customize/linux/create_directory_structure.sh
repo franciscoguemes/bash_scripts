f005252db1e23c024a0c65f292687c46574cb8f6
franciscoguemes <francisco.jose.guemes.sevilla@gmail.com>
2021-06-04 19:14:42 +0200

    Modify create_directory_structure script

--- new_structure/customize/linux/create_directory_structure.sh
+++ new_structure/customize/linux/create_directory_structure.sh
@@ -1,9 +1,11 @@
 
 
+# Create global directories (Execute script as sudo)
+mkdir -p /usr/lib/jvm
 
-# Create directories
+# Create user directories
 mkdir -p $HOME/bin
 mkdir -p $HOME/.config
 mkdir -p $HOME/development/{apache-ant,apache-maven,flyway,eclipse,netbeans}
-mkdir -p $HOME/git/{work,$USER,other}
+mkdir -p $HOME/git/{$COMPANY,$USER,other}
 mkdir -p $HOME/workspaces/{eclipse,netbeans}
