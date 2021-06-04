9716a36c6c64274cff2c1bc4f50b3a125ae3d1ff
franciscoguemes <francisco.jose.guemes.sevilla@gmail.com>
2021-06-04 19:13:49 +0200

    Modify linux_setup.sh script

--- new_structure/customize/linux_setup.sh
+++ new_structure/customize/linux_setup.sh
@@ -1,3 +1,11 @@
 
 # TODO: Create a DryRun option and a Verbose option
 #
+
+# TODO: Make this script interactive
+# Ask for the following values:
+#   author: It appears in the templates of the scripts. Use this value to interpolate the template scritps
+#   email: It appears in the templates of the scripts. Use this value to interpolate the template scritps
+#   company: A global variable called COMPANY will be created. This variable will be used for creating directories and other things.
+
+
