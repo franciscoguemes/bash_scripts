40a5c335138230e711f8c9ad80a85bdfada8fa89
franciscoguemes <francisco.jose.guemes.sevilla@gmail.com>
2021-06-08 01:18:04 +0200

    Add comment in the script

--- customize/linux/create_directory_structure.sh
+++ customize/linux/create_directory_structure.sh
@@ -1,9 +1,20 @@
-
+#!/usr/bin/env bash
+####################################################################################################
+#Script Name	: create_directory_structure.sh                                                                                             
+#Description	: Creates the directory structure on which the rest of the installation will rely.
+#                                                                                 
+#Args           :                                                                                           
+#Author       	: Francisco Güemes                                                
+#Email         	: francisco@franciscoguemes.com                                           
+#See also	    : 
+# 
+####################################################################################################
 
 # Create global directories (Execute script as sudo)
 mkdir -p /usr/lib/jvm
 
 # Create user directories
+mkdir -p $HOME/{Downloads,Templates,Documents,Videos,Music,Pictures}
 mkdir -p $HOME/bin
 mkdir -p $HOME/.config
 mkdir -p $HOME/development/{apache-ant,apache-maven,flyway,eclipse,netbeans,gradle}
