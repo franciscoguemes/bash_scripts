c144e5b770137ebd8c44ea6930c8b1d931576dbf
franciscoguemes <francisco.jose.guemes.sevilla@gmail.com>
2021-06-07 19:22:55 +0200

    Modify script for creating directories

--- new_structure/customize/linux/create_directory_structure.sh
+++ new_structure/customize/linux/create_directory_structure.sh
@@ -6,6 +6,6 @@ mkdir -p /usr/lib/jvm
 # Create user directories
 mkdir -p $HOME/bin
 mkdir -p $HOME/.config
-mkdir -p $HOME/development/{apache-ant,apache-maven,flyway,eclipse,netbeans}
+mkdir -p $HOME/development/{apache-ant,apache-maven,flyway,eclipse,netbeans,gradle}
 mkdir -p $HOME/git/{$COMPANY,$USER,other}
 mkdir -p $HOME/workspaces/{eclipse,netbeans}
