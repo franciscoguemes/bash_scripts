f338662b614327f0b1ba6090cc10f7df0913cf66
franciscoguemes <francisco.jose.guemes.sevilla@gmail.com>
2021-08-19 12:37:06 +0200

    Add script for creating ssh_keys

--- customize/linux/create_global_variables.sh
+++ customize/linux/create_global_variables.sh
@@ -1,5 +1,5 @@
 
-# Define the global variable COMPANY and ask for the value at the beginning
+# TODO: Define the global variable COMPANY and ask for the value at the beginning
 
 # https://unix.stackexchange.com/questions/21598/how-do-i-set-a-user-environment-variable-permanently-not-session
 # https://askubuntu.com/questions/866161/setting-path-variable-in-etc-environment-vs-profile
