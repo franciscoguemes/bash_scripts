a08443dbeb8bb1cc1095c825a3abd319ef4fb21a
franciscoguemes <E2018vasemcd>
2023-02-06 10:21:55 +0200

    Add helper scripts

---
+++ general_purpose/python/sdkmanrc/list_java.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/72416650/how-to-run-sdkman-on-shell-script
+# https://stackoverflow.com/questions/46262511/how-to-use-sdkman-to-install-packages-from-within-scripts
+
+. /home/francisco/.sdkman/bin/sdkman-init.sh
+sdk list java
