7a3befa54b012bf50487ab878b3209ce3ffef1af
franciscoguemes <francisco@franciscoguemes.com>
2023-02-06 10:36:05 +0200

    Blank line

--- general_purpose/python/sdkmanrc/list_gradle.sh
+++ general_purpose/python/sdkmanrc/list_gradle.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # https://stackoverflow.com/questions/72416650/how-to-run-sdkman-on-shell-script
 # https://stackoverflow.com/questions/46262511/how-to-use-sdkman-to-install-packages-from-within-scripts
 
